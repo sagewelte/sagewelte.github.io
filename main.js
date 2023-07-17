03fbe8438caf4008ff26a5cee92f415389d238cd
sjfox19 <139179087+sjfox19@users.noreply.github.com>
2023-07-17 14:02:02 -0400

    update 7/17

---
+++ main.js
@@ -0,0 +1,9 @@
+anime({
+    targets: "#name-svg path",
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutCubic',
+    duration: 3000,
+    direction: 'alternate',
+    loop: false,
+    fill: '#000000'
+})
---
+++ ue/main.js
@@ -0,0 +1,10 @@
+var map = L.map('map').setView([42.9529392,-78.8336695], 13);
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20
+}).addTo(map);
+
+L.geoJSON(docks, 
+    {style: tStyle}).addTo(map)
